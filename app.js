3f4fa5eabfa8be285a32ba9717e84eabf9c78557
LuizaC <56137562+luizagit@users.noreply.github.com>
2021-09-30 14:32:16 +0100

    creating views directoy and and changing app.js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
 
 
 app.get('/', (req, res) => {
-    res.send('HELLO YELP CAMP!')
+    res.render('home')
 })
 app.listen(3000, () => {
     console.log('Serving on port 3000')
