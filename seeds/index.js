3fb5693b4f87ccc464d3c6d63d23be24dc431f41
LuizaC <56137562+luizagit@users.noreply.github.com>
2021-11-06 00:09:05 +0000

    added campground index and setting up routes for campgrounds

--- seeds/index.js
+++ seeds/index.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
-const Campground = require('./models/campground');
+const Campground = require('../models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
