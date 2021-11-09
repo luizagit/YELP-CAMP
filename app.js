e93a9ff78bc3d23b3e407c19fa06c7bdda4380ee
LuizaC <56137562+luizagit@users.noreply.github.com>
2021-11-09 15:39:11 +0000

    make a new campground form

--- app.js
+++ app.js
@@ -25,10 +25,13 @@ app.get('/campgrounds', async(req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds })
 })
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+})
 app.get('/campgrounds/:id', async(req, res, ) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', { campground });
-})
+});
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
