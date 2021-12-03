6a586b78f235c91751c02d57f8fb605cd8e0eb68
LuizaC <56137562+luizagit@users.noreply.github.com>
2021-12-03 01:00:20 +0000

    implemented basic infrastructure for error handling

--- app.js
+++ app.js
@@ -35,10 +35,14 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 })
 
-app.post('/campgrounds', async(req, res) => {
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`)
+app.post('/campgrounds', async(req, res, next) => {
+    try {
+        const campground = new Campground(req.body.campground);
+        await campground.save();
+        res.redirect(`/campgrounds/${campground._id}`)
+    } catch (e) {
+        next(e);
+    }
 })
 
 app.get('/campgrounds/:id', async(req, res, ) => {
@@ -62,7 +66,9 @@ app.delete('/campgrounds/:id', async(req, res) => {
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
 })
-
+app.use((err, req, res, next) => {
+    res.send('Oh, something went wrong!')
+})
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
