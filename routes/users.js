0786e7426b59965ef69f99b00ea7fe6946fa8b3a
LuizaC <56137562+luizagit@users.noreply.github.com>
2022-04-14 13:40:05 +0100

    fixed register route-Login  funtion on req.

--- routes/users.js
+++ routes/users.js
@@ -12,8 +12,12 @@ router.post('/register', catchAsync(async(req, res, next) => {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
-        req.flash('success', 'Welcome to YELP-CAMP');
-        res.redirect('/campgrounds');
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            req.flash('success', 'Welcome to YELP-CAMP');
+            res.redirect('/campgrounds');
+        })
+
     } catch (e) {
         req.flash('error', e.message);
         res.redirect('register');
