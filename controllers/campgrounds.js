2b64beff018cf34eb7effdb3bd71ae2e8505ac5a
LuizaC <56137562+luizagit@users.noreply.github.com>
2022-05-26 15:14:34 +0100

    storing uploaded photos img links in Mongo

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,5 @@
 const Campground = require('../models/campground');
+const { cloudinary } = require("../cloudinary");
 
 module.exports.index = async(req, res) => {
     const campgrounds = await Campground.find({});
@@ -9,8 +10,10 @@ module.exports.renderNewForm = (req, res) => {
 }
 module.exports.createCampground = async(req, res, next) => {
     const campground = new Campground(req.body.campground);
+    campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.author = req.user._id;
     await campground.save();
+    console.log(campground);
     req.flash('success', 'Successfully made a new campground!');
     res.redirect(`/campgrounds/${campground._id}`)
 }
